2303faeb481cf6773666bcebe3c5086f5bd4a005
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-08-28 19:27:04 +0300

    - Add function printSomething - Remove println

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello git");
-        System.out.println("This is  a new change");
+    }
+
+    public void printSomething(){
+        System.out.println("new function");
     }
 }
