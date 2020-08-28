37073d48375c09c4efe435a1ea493970ea77cf71
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-08-28 19:30:36 +0300

    Remove function printSomething

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,4 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello git");
     }
-
-    public void printSomething(){
-        System.out.println("new function");
-    }
 }
